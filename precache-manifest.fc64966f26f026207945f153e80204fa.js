2bda70867ef7e1944659d2b8c07792564b41603d
maheshchilukamari <mahesh.chilkamari13@gmail.com>
2022-06-22 23:10:37 +0530

    Updates

---
+++ precache-manifest.fc64966f26f026207945f153e80204fa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f309aa6c5162f660b9933554c1f3eb6c",
+    "url": "/priyanka/index.html"
+  },
+  {
+    "revision": "5642f4cba789498ced27",
+    "url": "/priyanka/static/css/main.40551326.chunk.css"
+  },
+  {
+    "revision": "2a8ce47eb7bfc95b1c0d",
+    "url": "/priyanka/static/js/2.78186614.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/priyanka/static/js/2.78186614.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5642f4cba789498ced27",
+    "url": "/priyanka/static/js/main.e01a67f5.chunk.js"
+  },
+  {
+    "revision": "cebbd4d7ab60e15f3eb6",
+    "url": "/priyanka/static/js/runtime-main.0c67a3ee.js"
+  },
+  {
+    "revision": "de28f71ec6eb8dfda2e68d2211ee49eb",
+    "url": "/priyanka/static/media/AvenirNextLTPro-Demi.de28f71e.otf"
+  }
+]);
